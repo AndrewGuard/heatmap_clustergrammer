e34245b54377bf69d569a64ed387d4ac06cbf20f
Andrew Guard <aguard@anl.gov>
2018-07-18 14:57:57 -0500

    fix breaking change

--- clustergrammer.js
+++ clustergrammer.js
@@ -14565,9 +14565,21 @@ var Clustergrammer =
 	    tip = null;
 	  }
 
-	  document.getElementsByClassName('clust_group').addEventListener('click', function(event) {
-		console.log(event)
-	  });
+	 //  var closeIcons=document.getElementsByClassName('.monique-close-icon');  
+
+		// function closeBigImgAndContainer()
+		// {
+		//     MoniqueDiv.style.display= "none";
+		//     currentBigImageToDisplay.style.display="none";
+		// };
+
+		// for (i = 0; i < closeIcons.length; i++) {
+		//     closeIcons[i].addEventListener("click", closeBigImgAndContainer);
+		// }
+
+	 //  document.getElementsByClassName('clust_group').addEventListener('click', function(event) {
+		// console.log(event)
+	 //  });
 
 	  // gather a subset of row data from the matrix or use all rows
 	  var matrix_subset = [];
